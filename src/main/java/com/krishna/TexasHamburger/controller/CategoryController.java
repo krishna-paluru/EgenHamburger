c56a6c5278b7c31e2fdbb79d09ba42a76f0a4853
krishna-paluru <krishnachaitany.paluru@mavs.uta.edu>
2022-01-15 01:31:28 -0500

    changed docker-compose.yml

--- src/main/java/com/krishna/TexasHamburger/controller/CategoryController.java
+++ src/main/java/com/krishna/TexasHamburger/controller/CategoryController.java
@@ -74,10 +74,6 @@ public class CategoryController {
         categoryService.deleteCategoryById(id);
         return "category deleted !!";
     }
-    @GetMapping("/test")
-    public String test()
-    {
-        return "Hello There!!";
-    }
+
 
 }
