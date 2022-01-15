062177179d8498e6888e9503814d4170236f1c38
krishna-paluru <krishnachaitany.paluru@mavs.uta.edu>
2022-01-14 22:51:14 -0500

    changed

--- src/main/java/com/krishna/TexasHamburger/config/WebSecurityConfig.java
+++ src/main/java/com/krishna/TexasHamburger/config/WebSecurityConfig.java
@@ -6,37 +6,53 @@
 //import org.springframework.http.HttpMethod;
 //import org.springframework.security.authentication.AuthenticationProvider;
 //import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 //import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.builders.WebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 //import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 //import org.springframework.security.core.userdetails.UserDetailsService;
 //import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 //
 //@Configuration
+//@EnableWebSecurity
 //public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 //
 //    @Autowired
 //    private UserDetailsService userDetailsService;
-//    @Bean
-//    AuthenticationProvider authenticationProvider(){
+////    @Bean
+////    AuthenticationProvider authenticationProvider(){
+////
+////        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+////        provider.setUserDetailsService(userDetailsService);
+////        provider.setPasswordEncoder(new BCryptPasswordEncoder());
+////        return provider;
+////    }
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        super.configure(auth);
+//        auth.userDetailsService(userDetailsService);
 //
-//        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-//        provider.setUserDetailsService(userDetailsService);
-//        provider.setPasswordEncoder(new BCryptPasswordEncoder());
-//        return provider;
 //    }
 //
 //
 //    @Override
 //    protected void configure(HttpSecurity http) throws Exception {
-//        http.csrf().disable()
-//        .authorizeRequests()
-//                .antMatchers("/TexasHamburger/order/**")
-//                .hasAuthority("User")
-//                .antMatchers("/TexasHamburger/cancelOrder/**")
-//                .hasAuthority("User")
-//                .anyRequest()
-//                .authenticated()
-//                .and()
-//                .httpBasic();
+//        http.authorizeRequests()
+//                .antMatchers("/TexasHamburger/order")
+//                .hasRole("User")
+//                .antMatchers("/")
+//                .permitAll()
+//                .and().formLogin();
 //    }
+//
+//
+//    @Bean
+//    @Primary
+//    public BCryptPasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//
+//
 //}
